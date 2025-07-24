3f3f7f6aebcb1c2cecfc95d6d1679bd6659e6ade
Lazyguylol <m66361544@gmail.com>
2025-07-24 20:27:49 +0300

    Update Banned.lua

--- Banned.lua
+++ Banned.lua
@@ -1,13 +1,1 @@
-return function(player)
-    local blacklistedUsers = {
-        ["allexecutorfan123"] = true,
-        ["lakabsjs96"] = true,
-        ["AnthonyJJK10"] = true,
-        ["vrroromm"] = true,
-        ["Treezz_w"] = true
-    }
-    if blacklistedUsers[player.Name] then
-        return true
-    end
-    return false
-end
+Couldn't code the idea.
